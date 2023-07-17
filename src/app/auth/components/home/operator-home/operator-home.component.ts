e2b206180bc448a0fbf56dfe80f89a94ba4dddb9
mihailotomi <mihailotomi2@gmail.com>
2023-07-17 12:38:29 +0200

    create home component for all roles

---
+++ src/app/auth/components/home/operator-home/operator-home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operator-home',
+  templateUrl: './operator-home.component.html',
+  styleUrls: ['./operator-home.component.scss'],
+})
+export class OperatorHomeComponent {}
