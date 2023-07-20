0312623c1f42ce02261382bac1da848041ee8c0b
mihailotomi <mihailotomi2@gmail.com>
2023-07-20 14:55:01 +0200

    create placeholder navbar

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,12 +3,24 @@ import { Role } from '../../auth/enums/role';
 import { AuthService } from '../../auth/services/auth.service';
 import { Observable } from 'rxjs';
 import { User } from '../../auth/models/user';
+import { UserHomeComponent } from './user-home/user-home.component';
+import { OperatorHomeComponent } from './operator-home/operator-home.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
+  imports: [
+    UserHomeComponent,
+    OperatorHomeComponent,
+    AdminHomeComponent,
+    NavbarComponent,
+    CommonModule,
+  ],
 })
 export class HomeComponent {
   currentUser$: Observable<User | null>;
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProfileDropdownComponent } from './profile-dropdown/profile-dropdown.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   standalone: true,
+  imports: [ProfileDropdownComponent],
 })
 export class NavbarComponent {}
